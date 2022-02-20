7a424308854e21e1d0ef03f4bd2d931576645403
tfukushi <tfukushi@tfukushinoMacBook-Air.local>
2022-02-20 17:15:03 +0900

    バックグラウンドでブックマークを取得する

---
+++ chrome-extensions-bookmarks-sorting/README.md
@@ -0,0 +1,8 @@
+# ブックマークをソートする拡張機能
+
+参考：
+https://github.com/tukushi12/chrome-extensions-samples/tree/main/examples/bookmarks
+
+## 使い方
+
+## その他
---
+++ chrome-extensions-bookmarks-sorting/background.js
@@ -0,0 +1,14 @@
+// インストール時に実行し、`chrome.storage`に保存する
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.bookmarks.getTree().then((res) => {
+		console.log(res[0]["children"][0]["children"]);
+		console.log(res[0]["children"][0]["children"].length);
+		let setNodes = res[0]["children"][0]["children"].length;
+		chrome.storage.sync.set({ k: setNodes });
+	});
+});
+
+// ブックマークをすべて取得する
+function getTreeNodes() {
+	chrome.bookmarks.getTree();
+}
