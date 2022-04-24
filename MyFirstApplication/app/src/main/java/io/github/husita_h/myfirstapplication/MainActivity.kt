3e0c2ecfe6c0863903c1881e65f29fa459c80586
husita-h <taisei.fukushi1129@gmail.com>
2022-04-24 12:14:56 +0900

    ボタンのON/OFFによるアニメーションを追加

--- MyFirstApplication/app/src/main/java/io/github/husita_h/myfirstapplication/MainActivity.kt
+++ MyFirstApplication/app/src/main/java/io/github/husita_h/myfirstapplication/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
@@ -71,7 +73,11 @@ private fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false) }
 
     val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
     )
 
     Surface(
@@ -81,7 +87,7 @@ private fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)){
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             )
             {
                 Text(text = "Hello,")
