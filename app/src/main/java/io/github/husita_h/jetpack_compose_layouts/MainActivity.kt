b7c73d19d676588f93f1613b214ab10bfdc7d1f8
husita-h <taisei.fukushi1129@gmail.com>
2022-04-26 23:23:05 +0900

    隙間を開ける

--- app/src/main/java/io/github/husita_h/jetpack_compose_layouts/MainActivity.kt
+++ app/src/main/java/io/github/husita_h/jetpack_compose_layouts/MainActivity.kt
@@ -3,14 +3,12 @@ package io.github.husita_h.jetpack_compose_layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +47,11 @@ fun PhotographerCard() {
         ) {
             // Image goes here
         }
-        Column {
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
             Text("Alfred Sisley", fontWeight = FontWeight.Bold)
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
                 Text("3 minutes ago", style = MaterialTheme.typography.body2)
