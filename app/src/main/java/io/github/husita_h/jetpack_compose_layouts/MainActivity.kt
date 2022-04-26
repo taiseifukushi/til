a5fac595e4b3ca2aef2ea69edb1d5a82388fa270
husita-h <taisei.fukushi1129@gmail.com>
2022-04-26 23:21:12 +0900

    Previewを追加

--- app/src/main/java/io/github/husita_h/jetpack_compose_layouts/MainActivity.kt
+++ app/src/main/java/io/github/husita_h/jetpack_compose_layouts/MainActivity.kt
@@ -3,13 +3,18 @@ package io.github.husita_h.jetpack_compose_layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.husita_h.jetpack_compose_layouts.ui.theme.JetpackcomposelayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,10 +39,37 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun PhotographerCard() {
+    Row {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackcomposelayoutsTheme {
         Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    JetpackcomposelayoutsTheme {
+        PhotographerCard()
     }
 }
