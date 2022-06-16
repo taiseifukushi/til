929427688647037c7d9841561d27b8f06616f7d0
husita-h <taisei.fukushi1129@outlook.jp>
2022-06-17 01:24:19 +0900

    Initial commit

---
+++ kotlin-springboot-demo/src/test/kotlin/kotlinspringbootdemo/kotlinspringbootdemo/KotlinSpringbootDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinspringbootdemo.kotlinspringbootdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
