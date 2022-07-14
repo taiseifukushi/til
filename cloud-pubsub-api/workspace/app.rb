964fc1a3eb9fac53d8a63551b015bfe7ed5bc05a
husita-h <taisei.fukushi1129@gmail.com>
2022-07-15 01:02:35 +0900

    google-cloud-pubsub-v1をインストール

--- cloud-pubsub-api/workspace/app.rb
+++ cloud-pubsub-api/workspace/app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require "sinatra/reloader"
+require "google/cloud/pubsub/v1"
+
+# before do
+#   response.headers['Access-Control-Allow-Origin'] = '*'
+# end
 
 get '/' do
-  'Hello, World!'
+  'Hello World!'
 end
+
+# 認証
+# https://cloud.google.com/pubsub/docs/authentication?hl=ja
