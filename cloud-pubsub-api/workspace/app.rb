8128972f6f7172c55b567eac491ae409a583cbc1
husita-h <taisei.fukushi1129@gmail.com>
2022-07-17 01:48:17 +0900

    configファイルと.envファイルの設定を追加

--- cloud-pubsub-api/workspace/app.rb
+++ cloud-pubsub-api/workspace/app.rb
@@ -1,15 +1,22 @@
-require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sinatra/namespace'       # http://sinatrarb.com/contrib/namespace
 require 'sinatra/json'            # http://sinatrarb.com/contrib/json.html
+require 'sinatra/config_file'     # http://sinatrarb.com/contrib/config_file
 require 'google/cloud/pubsub/v1'
+if Sinatra::Base.environment == :development
+  require 'pry'
+  require 'dotenv'
+  Dotenv.load
+end
 
 class App < Sinatra::Base
   register Sinatra::Namespace
   configure :development do
     register Sinatra::Reloader
+    register Sinatra::ConfigFile
   end
+  config_file './local_config.yml'
 
   # CORS設定
   # before do
@@ -25,10 +32,6 @@ class App < Sinatra::Base
   # https://cloud.google.com/pubsub/docs/authentication?hl=ja
   # https://googleapis.dev/ruby/google-cloud-pubsub/latest/file.AUTHENTICATION.html#creating-a-service-account
 
-  # 環境変数呼び出し
-  # ENV["PUBSUB_PROJECT"]     = "project-pubsub-333815"
-  # ENV["PUBSUB_CREDENTIALS"] = "../../../../Auth"
-
   # Google::Cloud::PubSub.configure do |config|
   #   config.project_id  = "my-project-id"
   #   config.credentials = "path/to/keyfile.json"
@@ -36,6 +39,13 @@ class App < Sinatra::Base
   namespace '/api' do
     get '/' do
       json test_key: 'test_val'
+    end
+    get '/env' do
+      json({
+             # test_key1: ENV['PUBSUB_PROJECT'],
+             test_key1: settings.service_account_info['pubsub_project'],
+             test_key2: settings.service_account_info['pubsub_credentials']
+           })
     end
   end
 end
