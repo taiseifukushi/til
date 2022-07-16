bf815ad87a710a1086440be20cd0c03863510ac4
husita-h <taisei.fukushi1129@gmail.com>
2022-07-17 00:04:03 +0900

    rackを使ってサーバーを起動できるように変更

--- cloud-pubsub-api/workspace/app.rb
+++ cloud-pubsub-api/workspace/app.rb
@@ -1,14 +1,35 @@
 require 'sinatra'
+require 'sinatra/base'
 require "sinatra/reloader"
 require "google/cloud/pubsub/v1"
 
-# before do
-#   response.headers['Access-Control-Allow-Origin'] = '*'
-# end
-
-get '/' do
-  'Hello World!'
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  
+  # CORS設定
+  # before do
+  #   response.headers['Access-Control-Allow-Origin'] = '*'
+  # end
+  
+  get '/' do
+    'Hello World!'
+  end
+  
+  # ==========
+  # 認証
+  # https://cloud.google.com/pubsub/docs/authentication?hl=ja
+  # https://googleapis.dev/ruby/google-cloud-pubsub/latest/file.AUTHENTICATION.html#creating-a-service-account
+  
+  # 環境変数呼び出し
+  # ENV["PUBSUB_PROJECT"]     = "project-pubsub-333815"
+  # ENV["PUBSUB_CREDENTIALS"] = "../../../../Auth"
+  
+  # Google::Cloud::PubSub.configure do |config|
+  #   config.project_id  = "my-project-id"
+  #   config.credentials = "path/to/keyfile.json"
+  # end
+  
+  # 
 end
-
-# 認証
-# https://cloud.google.com/pubsub/docs/authentication?hl=ja
