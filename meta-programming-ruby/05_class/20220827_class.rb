839e81bb804a1b5fd0830a28b90431dd395dcfcb
husita-h <taisei.fukushi1129@gmail.com>
2022-08-28 23:58:51 +0900

    05 class_eval vs instance_eval

--- meta-programming-ruby/05_class/20220827_class.rb
+++ meta-programming-ruby/05_class/20220827_class.rb
@@ -18,10 +18,42 @@ p c.new.class
 
 class C
   def m1
+    def m2;end
   end
 end
 
 class D < C; end
 d = D.new
 p d.m1
-p C.instance_method(false)
+p C.instance_methods(false)
+# ➜  05_class git:(main) ✗ be ruby 20220827_class.rb
+# :m2
+# [:m1, :m2]
+
+# https://docs.ruby-lang.org/ja/latest/method/Module/i/class_eval.html
+# class_eval
+# Module#class_evalで、そのクラスにインスタンスメソッドや特異クラスメソッドを追加する
+def add_method_to(a_class)
+  a_class.class_eval do
+    def m1
+      "add m1 method with class_eval"
+    end
+  end
+end
+
+add_method_to(String)
+p "teststring".m1
+# => "add m1 method with class_eval"
+p String.instance_methods
+# [..., :m1, ..., ...]
+p String.new.m1
+# => "add m1 method with class_eval"
+# クラスメソッドを追加したはずだけど、インスタンスメソッドが追加されている？
+# ???
+
+# 表現がわからない
+# >オブジェクトをオープンする
+# >クラス定義をオープンする
+# ???
+# このオブジェクトをオープンしたいが、クラスのことは気にしたい => instance_eval
+# ここでオープンクラスを使いたい => class_eval
