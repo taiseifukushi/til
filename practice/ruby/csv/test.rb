272701a88c75db467ecd8dfbf675ca8dfac8d0c3
husita-h <taisei.fukushi1129@gmail.com>
2022-09-02 23:31:21 +0900

    CSV.open & CSV#select

--- practice/ruby/csv/test.rb
+++ practice/ruby/csv/test.rb
@@ -1,37 +1,25 @@
 # https://docs.ruby-lang.org/ja/latest/class/CSV.html
 require "csv"
+require "pry"
 
-changed_item_csv_path = "changed_item.csv"
+item_changed_csv_path = "item_changed.csv"
 base_csv_path         = "base.csv"
-# options = { headers: true, return_headers: true, write_headers: true }
+base_updated_csv_path  = "base_updated.csv"
+item_changed_csv      = CSV.read(item_changed_csv_path, headers: true) # => Array
+base_csv              = CSV.read(base_csv_path, headers: true) # => Array# => Array
 
-changed_item_csv = CSV.read(changed_item_csv_path, headers: true) # => Array
-base_csv   = CSV.read(base_csv_path, headers: true) # => Array# => Array
-
-# changed_item_csv.each do |row|
-#   p row
-#   p row.class
-#   p row["key"]
-# end
-
-base_csv.each do |base_csv_row|
-  changed_item_csv.each do |changed_item_csv_row|
-    if base_csv_row["key"] == changed_item_csv_row["key"]
-      p "matched."
-      p changed_item_csv_row["key"]
+CSV.open(base_updated_csv_path, "wb") do |csv|
+  base_csv.each do |base_csv_row|
+    match_row = item_changed_csv.select do |item_changed_csv_row|
+      item_changed_csv_row["key"] == base_csv_row["key"]
     end
-  end
-end
 
-p "==="
-
-base_csv.each do |base_csv_row|
-  changed_item_csv.each do |changed_item_csv_row|
-    if base_csv_row["key"] == changed_item_csv_row["key"]
-      p "matched."
-      p changed_item_csv_row["key"]
+    unless match_row.empty?
+      # binding.pry
+      base_csv_row["value"] = match_row[0]["value"]
     end
+
+    csv << base_csv_row
   end
+  p "successed."
 end
-
-
