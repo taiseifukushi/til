493eaebe6968fd409485fe868ac1c2dd8b4ab17f
husita-h <taisei.fukushi1129@gmail.com>
2022-09-03 23:50:36 +0900

    csvを書き出すクラスを作成

--- practice/ruby/csv/test.rb
+++ practice/ruby/csv/test.rb
@@ -1,28 +1,46 @@
 # https://docs.ruby-lang.org/ja/latest/class/CSV.html
 # https://docs.ruby-lang.org/ja/latest/library/csv.html
-require "csv"
-require "pry"
 
-item_changed_csv_path = "item_changed.csv"
-base_csv_path         = "base.csv"
-base_updated_csv_path = "base_updated.csv"
-item_changed_csv      = CSV.read(item_changed_csv_path, headers: true)
-base_csv              = CSV.read(base_csv_path, headers: true)
-header                = ["key", "value" , "arg1", "arg2", "arg3"]
+class CsvGenerator
+  require "csv"
+  require "pry"
 
-CSV.open(base_updated_csv_path, "w") do |csv|
-  csv << header
-  base_csv.each do |base_csv_row|
-    match_row = item_changed_csv.select do |item_changed_csv_row|
-      item_changed_csv_row["key"] == base_csv_row["key"]
-    end
+  attr_reader :item_changed_csv_path, :base_csv_path, :base_updated_csv_path
 
-    unless match_row.empty?
-      base_csv_row["value"] = match_row[0]["value"]
-    end
-
-    csv << base_csv_row
+  def initialize
+    @item_changed_csv_path = "item_changed.csv"
+    @base_csv_path         = "base.csv"
+    @base_updated_csv_path = "base_updated.csv"
   end
-  p "successed."
-  p csv
+
+  def generate_csv(&header)
+    CSV.open(@base_updated_csv_path, "w") do |csv|
+      csv << header.call
+      read_csv_file(@base_csv_path).each do |base_csv_row|
+        match_row = read_csv_file(@item_changed_csv_path).select do |item_changed_csv_row|
+          item_changed_csv_row["key"] == base_csv_row["key"]
+        end
+        
+        unless match_row.empty?
+          base_csv_row["value"] = match_row[0]["value"]
+        end
+        
+        csv << base_csv_row
+      end
+      p "successed."
+      p csv
+    end
+  end
+
+  private
+
+  def read_csv_file(path)
+    CSV.read(path, headers: true)
+  end
 end
+
+simple_header = lambda do
+  ["key", "value" , "arg1", "arg2", "arg3"]
+end
+generator = CsvGenerator.new
+generator.generate_csv(&simple_header)
