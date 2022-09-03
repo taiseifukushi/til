922703399e902f276dd232fa440e053a8e44f79b
husita-h <taisei.fukushi1129@gmail.com>
2022-09-03 09:04:14 +0900

    headerを追加

--- practice/ruby/csv/test.rb
+++ practice/ruby/csv/test.rb
@@ -1,25 +1,28 @@
 # https://docs.ruby-lang.org/ja/latest/class/CSV.html
+# https://docs.ruby-lang.org/ja/latest/library/csv.html
 require "csv"
 require "pry"
 
 item_changed_csv_path = "item_changed.csv"
 base_csv_path         = "base.csv"
-base_updated_csv_path  = "base_updated.csv"
-item_changed_csv      = CSV.read(item_changed_csv_path, headers: true) # => Array
-base_csv              = CSV.read(base_csv_path, headers: true) # => Array# => Array
+base_updated_csv_path = "base_updated.csv"
+item_changed_csv      = CSV.read(item_changed_csv_path, headers: true)
+base_csv              = CSV.read(base_csv_path, headers: true)
+header                = ["key", "value" , "arg1", "arg2", "arg3"]
 
-CSV.open(base_updated_csv_path, "wb") do |csv|
+CSV.open(base_updated_csv_path, "w") do |csv|
+  csv << header
   base_csv.each do |base_csv_row|
     match_row = item_changed_csv.select do |item_changed_csv_row|
       item_changed_csv_row["key"] == base_csv_row["key"]
     end
 
     unless match_row.empty?
-      # binding.pry
       base_csv_row["value"] = match_row[0]["value"]
     end
 
     csv << base_csv_row
   end
   p "successed."
+  p csv
 end
