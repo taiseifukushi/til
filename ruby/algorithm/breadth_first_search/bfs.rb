089ef0cd2d693eb34b7a37e3aebe4e40b456aaf1
husita-h <taisei.fukushi1129@gmail.com>
2022-09-15 23:58:41 +0900

    algorithm bfs dfs 2

--- ruby/algorithm/breadth_first_search/bfs.rb
+++ ruby/algorithm/breadth_first_search/bfs.rb
@@ -1,3 +1,7 @@
+# 幅優先探索
+# 出発地点に近い頂点から順番に調べる
+
+# ==========
 #        0
 #        |
 #    +-------+
@@ -5,6 +9,7 @@
 #  +-1-+   +-2-+
 #  |   |   |   |
 #  3   4   5   6
+# ==========
 
 
 class Node
@@ -15,36 +20,43 @@ class Node
     @visited_flag = false
   end
 
+  # 訪れたノードの名前を出力
   def visit
-    p @name
+    p "Here is: #{@name}"
   end
 
-  def adjacent()
+  # 連結するノードを取得
+  def adjacent
     @nodes
   end
 
+  # 訪れたノードにフラグを立てる
   def visited
     @visited_flag = true
   end
 
   def visited?
-    return @visited_flag
+    @visited_flag
   end
 end
 
-
+# ==========
+# 探索実行
 def search(node)
+  # https://docs.ruby-lang.org/ja/latest/class/Thread=3a=3aQueue.html
   queue = Queue.new
-
   node.visit
   node.visited
-
+  # queにnodeをエンキューする
   queue.enq(node)
 
+  # queuが空になるまでwhile内の処理を実行する
   while !queue.empty?
-    node = queue.deq()
-    node.adjacent.each do | child |
-      if child.visited? == false
+    # queからnodeを一つデキューする
+    node = queue.deq
+    # nodeに連結している子ノードに対してブロック内の処理を実行する
+    node.adjacent.each do |child|
+      unless child.visited?
         child.visit
         child.visited
         queue.enq(child)
@@ -53,6 +65,7 @@ def search(node)
   end
 end
 
+# ノードを登録する
 node3 = Node.new(3)
 node2 = Node.new(2)
 node1 = Node.new(1, node2, node3)
@@ -61,4 +74,13 @@ node6 = Node.new(6)
 node4 = Node.new(4, node5, node6)
 node0 = Node.new(0, node1, node4)
 
+# node0: 頂点
 search(node0)
+# ➜  breadth_first_search git:(main) ✗ be ruby bfs.rb
+# "Here is: 0"
+# "Here is: 1"
+# "Here is: 4"
+# "Here is: 2"
+# "Here is: 3"
+# "Here is: 5"
+# "Here is: 6"
--- ruby/algorithm/depth_first_search/dfs.rb
+++ ruby/algorithm/depth_first_search/dfs.rb
@@ -1,3 +1,7 @@
+# 深さ優先探索
+# 進めるだけ進み、行き止まりに到達したら一歩戻る
+
+# ==========
 #        0
 #        |
 #    +-------+
@@ -5,6 +9,7 @@
 #  +-1-+   +-4-+
 #  |   |   |   |
 #  2   3   5   6
+# ==========
 
 
 class Node
@@ -15,34 +20,43 @@ class Node
     @visited_flag = false
   end
 
+  # 訪れたノードの名前を出力
   def visit
-    p @name
+    p "Here is: #{@name}"
   end
 
-  def adjacent()
+  # 連結するノードを取得
+  def adjacent
     @nodes
   end
 
+  # 訪れたノードにフラグを立てる
   def visited
     @visited_flag = true
   end
 
   def visited?
-    return @visited_flag
+    @visited_flag
   end
 end
 
+# ==========
+# 探索実行
 def search(node)
   node.visit
   node.visited
-
-  node.adjacent.each do | child |
-    if child.visited? == false
+  # nodeに連結している子ノードに対してブロック内の処理を実行する
+  node.adjacent.each do |child|
+    # そのノードのフラグがfalseであれば、ブロック内の処理を実行
+    unless child.visited?
+      # その子ノードに対してsearch()を再帰的に呼び出す
+      # さらにその子ノード、孫ノードへの処理を進める
       search(child)
     end
   end
 end
 
+# ノードを登録する
 node3 = Node.new(3)
 node2 = Node.new(2)
 node1 = Node.new(1, node2, node3)
@@ -51,4 +65,13 @@ node6 = Node.new(6)
 node4 = Node.new(4, node5, node6)
 node0 = Node.new(0, node1, node4)
 
+# node0: 頂点
 search(node0)
+# ➜  depth_first_search git:(main) ✗ be ruby dfs.rb
+# "Here is: 0"
+# "Here is: 1"
+# "Here is: 2"
+# "Here is: 3"
+# "Here is: 4"
+# "Here is: 5"
+# "Here is: 6"
