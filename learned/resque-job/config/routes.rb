f09f5bd2a838a5281a80b83efd5bf9bb3f625c61
husita-h <taisei.fukushi1129@gmail.com>
2022-10-30 13:55:55 +0900

    rootを/resqueにリダイレクト

--- learned/resque-job/config/routes.rb
+++ learned/resque-job/config/routes.rb
@@ -1,6 +1,7 @@
 require 'resque/server'
 
 Rails.application.routes.draw do
+  root to: redirect("/resque")
   get 'users/:name', :to => 'users#show'
   mount Resque::Server.new, :at => "/resque"
 end
